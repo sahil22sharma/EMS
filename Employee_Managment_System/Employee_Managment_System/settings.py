c67ace4e1b565ebf6c08349ed4c925adbfedbecf
riteshborikar1010 <riteshborikar1010@gmail.com>
2024-12-18 15:13:17 +0530

    admin dashboard

--- Employee_Managment_System/Employee_Managment_System/settings.py
+++ Employee_Managment_System/Employee_Managment_System/settings.py
@@ -138,4 +138,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'profile.UserProfile'
+# AUTH_USER_MODEL = 'profile.UserProfile'
