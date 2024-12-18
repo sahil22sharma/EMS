322187f70b72cd86c455e12e1a2881a43138f670
riteshborikar1010 <riteshborikar1010@gmail.com>
2024-12-18 10:50:12 +0530

    update the html

--- Employee_Managment_System/EMS/urls.py
+++ Employee_Managment_System/EMS/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.homepage),
+    path('',views.homepage,name="home"),
 
     # Signup Routes
     path('adminReg/',views.adminReg, name='adminReg'),
     path('employeeReg/',views.employeeRegistration, name='employeeReg'),
     path('managerReg/',views.managerRegistration, name='managerReg'),
+    path('saveAdmin/',views.saveAdmin,name='saveAdmin'),
+    path('adminLogin/',views.loginAdmin,name='adminLogin')
 ]
