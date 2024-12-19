90f72e955ed03d3f19ac1911592220bce778cb8b
Sahil Sharma <sahil.s.sharma22@gmail.com>
2024-12-19 15:14:34 +0530

    Manager and Employee Dashboard

--- Employee_Managment_System/EMS/urls.py
+++ Employee_Managment_System/EMS/urls.py
@@ -29,5 +29,10 @@ urlpatterns = [
     path('adminDash/managerrequest', views.managerrequest,name='managerrequest'),
     path('adminDash/employeelist', views.employeelist,name='employeelist'),
     path('adminDash/employeerequest', views.employeerequest,name='employeerequest'),
-
+    path('employeeDash/',views.baseEmployee),
+    path('employeeDash/Index', views.employeeIndexPage,name='employeeDash'),
+    path('managerDash/',views.baseManager),
+    path('managerDash/Index', views.managerIndexPage,name='managerDash'),
+    path('managerDash/employeelist', views.managerEmployeelist,name='managerEmployeelist'),
+    path('managerDash/employeerequest', views.managerEmployeerequest,name='managerEmployeerequest'),
 ]
--- Employee_Managment_System/EMS/views.py
+++ Employee_Managment_System/EMS/views.py
@@ -619,3 +619,21 @@ def employeelist(request):
 
 def employeerequest(request):
     return render(request, "EMSadmin/employeeRequest.html")
+
+def baseEmployee(request):
+    return render(request, "employee/base.html")
+
+def employeeIndexPage(request):
+    return render(request,"employee/employeeDash.html")
+
+def baseManager(request):
+    return render(request, "manager/base.html")
+
+def managerIndexPage(request):
+    return render(request,"manager/managerDash.html")
+
+def managerEmployeelist(request):
+    return render(request, "manager/employeeList.html")
+
+def managerEmployeerequest(request):
+    return render(request, "manager/employeeRequest.html")
